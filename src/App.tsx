68050940d7418a1380549577cec6b40e458e4fad
terutika0604 <terutika0604@gmail.com>
2023-02-09 14:05:57 +0900

    muiインストール

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import Button from '@mui/material/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +20,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+        <AccessAlarmIcon />
+        <Button variant="contained">Hello World</Button>
     </div>
   );
 }
