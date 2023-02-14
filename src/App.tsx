27631f1c37ff11f4d0f7004ef86847ca7d28b90a
terutika0604 <terutika0604@gmail.com>
2023-02-14 10:45:04 +0900

    コードフォーマット

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-import AddIcon from '@mui/icons-material/Add';
-import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
-import PlaylistAddCheckIcon from '@mui/icons-material/PlaylistAddCheck';
-import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import AddIcon from "@mui/icons-material/Add";
+import DeleteForeverOutlinedIcon from "@mui/icons-material/DeleteForeverOutlined";
+import PlaylistAddCheckIcon from "@mui/icons-material/PlaylistAddCheck";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
 
-import Grid from '@mui/material/Grid';
+import Grid from "@mui/material/Grid";
 
-import Button from '@mui/material/Button';
-import Checkbox from '@mui/material/Checkbox';
-import TextField from '@mui/material/TextField';
+import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
+import TextField from "@mui/material/TextField";
 
 function App() {
   // todoのテキストの状態
   const [inputValue, setInputValue] = useState("");
   // todoを格納する配列
   const [todos, setTodos] = useState<Todo[]>([]);
-  
+
   // todoの型宣言
   type Todo = {
     inputValue: string;
-    id:number;
-    checked:boolean;
-  }
+    id: number;
+    checked: boolean;
+  };
 
   // todo追加
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if(inputValue == null) {
+    if (inputValue == null) {
       return;
     }
     // 新しいTODOを作成
@@ -37,98 +37,120 @@ function App() {
       inputValue: inputValue,
       id: todos.length,
       checked: false,
-    }
+    };
 
     setTodos([newTodo, ...todos]);
     setInputValue("");
-  }
+  };
 
   // todo編集
   const handleEdit = (id: number, inputValue: string) => {
     console.log(inputValue);
     const newTodos = todos.map((todo) => {
-      if(todo.id === id){
+      if (todo.id === id) {
         todo.inputValue = inputValue;
       }
       return todo;
     });
     setTodos(newTodos);
-  }
-  
+  };
+
   // inputの値をstateにset
   const handleChange = (text: React.SetStateAction<string>) => {
     setInputValue(text);
-  } 
+  };
 
   // チェックボックス操作
   const handleChecked = (id: number, checked: boolean) => {
     const newTodos = todos.map((todo) => {
-      if(todo.id === id){
+      if (todo.id === id) {
         todo.checked = !checked;
       }
       return todo;
     });
 
     setTodos(newTodos);
-  }
+  };
 
   // 削除処理
   const handleDelete = (id: number) => {
     // idが整合するもののみtrue
-    const newTodos = todos.filter((todo) => todo.id !== id )
+    const newTodos = todos.filter((todo) => todo.id !== id);
     setTodos(newTodos);
-  }
-  
+  };
+
   return (
     <div className="App">
-        <div>
-          <h2>Todo List<FormatListBulletedIcon sx={{ fontSize: 30, verticalAlign: "bottom", ml: 1}} /></h2>
-          <form onSubmit={(e) => {handleSubmit(e)}}>
-            <TextField 
-              label="TodoList"
-              variant="outlined"
-              onChange={(e) => handleChange(e.target.value)} 
-              value={inputValue}
-              inputProps={{ required: true }}
-            />
-            <Button 
-              type="submit"
-              variant="contained"
-              endIcon={<AddIcon />}
-              sx={{
-                mt: 1,
-                ml: 2,
-              }}
-            >ADD</Button>
-          </form>
-          <ul>
-            {todos.map((todo) => (
-              <li key={todo.id}>
-                <Grid container spacing={1} sx={{ textAlign: "center" }}>
-                  <Grid item xs={6}>
-                    <TextField 
-                      label="todo"
-                      variant="outlined"
-                      size="small"
-                      onChange={(e) => handleEdit(todo.id, e.target.value)} 
-                      value={todo.inputValue}
-                      disabled={todo.checked}
-                      margin="normal"
-                    />
-                  </Grid>
-                  <Grid item xs={2}>
-                    <Checkbox size="small" color="success" onChange={() => handleChecked(todo.id, todo.checked)} sx={{ mt: 2 }} />
-                  </Grid>
-                  <Grid item xs={4}>
-                    <Button variant="outlined" color="error" onClick={() => handleDelete(todo.id)} sx={{ mt: 2 }}>
-                      delete<DeleteForeverOutlinedIcon />
-                    </Button>
-                  </Grid>
+      <div>
+        <h2>
+          Todo List
+          <FormatListBulletedIcon
+            sx={{ fontSize: 30, verticalAlign: "bottom", ml: 1 }}
+          />
+        </h2>
+        <form
+          onSubmit={(e) => {
+            handleSubmit(e);
+          }}
+        >
+          <TextField
+            label="TodoList"
+            variant="outlined"
+            onChange={(e) => handleChange(e.target.value)}
+            value={inputValue}
+            inputProps={{ required: true }}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            endIcon={<AddIcon />}
+            sx={{
+              mt: 1,
+              ml: 2,
+            }}
+          >
+            ADD
+          </Button>
+        </form>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <Grid container spacing={1} sx={{ textAlign: "center" }}>
+                <Grid item xs={6}>
+                  <TextField
+                    label="todo"
+                    variant="outlined"
+                    size="small"
+                    onChange={(e) => handleEdit(todo.id, e.target.value)}
+                    value={todo.inputValue}
+                    disabled={todo.checked}
+                    margin="normal"
+                  />
                 </Grid>
-              </li>
-            ))}
-          </ul>
-        </div>
+                <Grid item xs={2}>
+                  <Checkbox
+                    size="small"
+                    color="success"
+                    onChange={() => handleChecked(todo.id, todo.checked)}
+                    sx={{ mt: 2 }}
+                  />
+                </Grid>
+                <Grid item xs={4}>
+                  <Button
+                    variant="outlined"
+                    color="error"
+                    onClick={() => handleDelete(todo.id)}
+                    sx={{ mt: 2 }}
+                  >
+                    delete
+                    <DeleteForeverOutlinedIcon />
+                  </Button>
+                </Grid>
+              </Grid>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
